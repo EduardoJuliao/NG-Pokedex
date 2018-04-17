3bb08293b3241d30ed8b8c8a9e0c0dc55c14dbc6
Eduardo Julião Junior <eduardo.juliaojr@hotmail.com>
2018-04-17 14:46:19 -0300

    Theme player

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -4,13 +4,40 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SoundService {
 
+    private themePlayer: Howl;
+
     constructor() { }
 
     public playSelect(): void{
         new Howl({
             src: ['assets/sounds/select.wav'],
             loop: false,
-            volume: 1
+            volume: 0.2
         }).play();
+    }
+
+    /**
+     * 
+     * 
+     * @returns {boolean} is playing
+     * @memberof SoundService
+     */
+    public playTheme():boolean{
+        if(!this.themePlayer){
+            this.themePlayer = new Howl({
+                src: ['assets/sounds/theme.wav'],
+                loop: true,
+                volume: 0.5,
+                html5: true
+            });
+        }
+
+        if(this.themePlayer.playing()){
+            this.themePlayer.pause();
+        }else{
+            this.themePlayer.play();
+        }
+
+        return this.themePlayer.playing();
     }
 }
